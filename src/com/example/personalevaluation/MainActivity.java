adce6ab0478c0d12ed8495ee0fa9bbddc2664aaf
jianhengzhou <jianhengzhou@163.com>
2014-12-05 16:50:25 +0800

    initial layout

--- src/com/example/personalevaluation/MainActivity.java
+++ src/com/example/personalevaluation/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.personalevaluation;
 
-import android.os.Bundle;
+import java.util.Date;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	private TextView dateTv;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		initialLayout();
+		
+		initialControl();
 	}
 
 	@Override
@@ -18,5 +27,18 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	private void initialLayout(){
+		dateTv = (TextView) findViewById(R.id.date_tv);
+	}
 
+	private void initialControl(){
+		
+		if(dateTv!=null){
+			//获取当前时间
+			Date date = new Date();
+			dateTv.setText(date.toLocaleString().split(" ")[0]);
+		}
+		
+	}
 }
